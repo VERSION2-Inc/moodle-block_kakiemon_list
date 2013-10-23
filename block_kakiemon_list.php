08df38160b58cb867dda0a91ce199371804b994e
Koichi Yamashita <k-yamashita@ver2.jp>
2013-10-23 16:32:13 +0900

    中間コミット

--- block_kakiemon_list.php
+++ block_kakiemon_list.php
@@ -1,1 +1,26 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
+
+class block_kakiemon_list extends block_base {
+	public function init() {
+		$this->name = 'list';
+	}
+
+	/**
+	 *
+	 * @return boolean[]
+	 */
+	public function applicable_formats() {
+		return array(
+				'course' => true
+		);
+	}
+
+	/**
+	 *
+	 * @return \stdClass
+	 */
+	public function get_content() {
+		return '';
+	}
+}
--- version.php
+++ version.php
@@ -4,3 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2013102300;
 $plugin->requires = 2010112400;
 $plugin->component = 'block_kakiemon_list';
+$plugin->dependencies = [
+	'mod_kakiemon' => 2013102100
+];
